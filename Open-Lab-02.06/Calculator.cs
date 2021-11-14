d937d66fb4645f048ccd9804bd62c162c2907b29
AdamHaluska <adam.haluska11@gmail.com>
2021-11-14 21:15:32 +0100

    hotovo!

--- Open-Lab-02.06/Calculator.cs
+++ Open-Lab-02.06/Calculator.cs
@@ -4,9 +4,12 @@ namespace Open_Lab_02._06
 {
     public class Calculator
     {
-        public bool Divisible(int number)
+         if (number % 100 == 0)
+                return true;
+            
         {
-            throw new NotImplementedException();
+           else
+                return false;
         }
     }
 }
